a773a72ab77385a3b413d41ce8fe34f631a0c842
seonyoung-jeon <seonyoung.jeon@injeinc.co.kr>
2024-06-19 10:51:22 +0900

    feat : admin 에서 공통 component 사용 테스트

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@baas/components-react';
 import { plusOne } from '@baas/utils';
 
 function App() {
   return (
     <>
       <h1>{plusOne(11)}</h1>
+      <Button />
     </>
   );
 }
